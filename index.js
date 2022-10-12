9eea8ba133080c65cb75c57c56bd621ef06d196e
Rohit Rao <rohit@rohitsrao.com>
2022-10-12 10:35:48 +0200

    Defined reducer and store from redux library

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const BUY_CAKE = 'BUY_CAKE';
+const redux = require('redux')
 
-function buyCake () {
+const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
+
+const buyCake = () => {
     return {
         type: BUY_CAKE,
-        info: 'First redux action'
     };
 }
+
+const buyIceCream = () => {
+    return {
+        type: BUY_ICECREAM,
+    }
+}
+
+
 
 // (previousState, action) => newState
 
 const initialState = {
-    numCakes: 10
+    numCakes: 10,
+    numIceCreams: 20
 }
 
 const reducer = (state = initialState, action) => {
@@ -20,8 +31,25 @@ const reducer = (state = initialState, action) => {
                 ... state,
                 numCakes: state.numCakes - 1
             }
+
+        case BUY_ICECREAM: 
+            return {
+                ... state,
+                numIceCreams: state.numIceCreams - 1
+            }
+        
         
         default:
             return state;
     }
 }
+
+const store = redux.createStore(reducer);
+console.log('Initial State', store.getState())
+const unsubscribe = store.subscribe(() => console.log('Updated State', store.getState()));
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+unsubscribe();
